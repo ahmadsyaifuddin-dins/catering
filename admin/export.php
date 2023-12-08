f018293539a55a7d49ffe4d3b999bd891448bbe2
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-08 09:20:52 +0800

    Initial commit

---
+++ admin/export.php
@@ -0,0 +1,79 @@
+<?php
+ob_start();
+include"../inc/config.php";
+?>
+
+<div class="container">
+	<h4>Laporan Penjualan</h4>
+	<?php
+	
+	?>
+	<div class="col-md-12">
+		<hr/>
+	</div>
+
+	<div class="row">
+		<table class="table table-striped" border="1">
+			<tr>
+				<th>#</th>
+				<th>Nama</th>
+				<th>Tanggal Tempo</th>
+				<th>Tanggal Pesan</th>
+				<th>Total</th>
+				<th>Ongkir</th>
+				<th>Status</th>
+			</tr>
+			<tbody>
+				<?php
+					$totalSemua = 0;
+					$totalOngkir = 0;
+					$no = 0;
+					$q = mysqli_query($konek, "Select pesanan.* from pesanan order by id desc") or die (mysqli_error());
+					while ($data = mysqli_fetch_object($q)) {
+						$totalHarga = 0;
+						$no++;
+						$q2 = mysqli_query($konek, "Select detail_pesanan.*, produk.harga from detail_pesanan INNER JOIN produk ON detail_pesanan.produk_id = produk.id where pesanan_id = '$data->id'") or die (mysqli_error());
+						while ($d = mysqli_fetch_object($q2)) {
+							$totalHarga += $d->harga * $d->qty;
+						}
+						$totalSemua += $totalHarga;
+						$totalOngkir += $data->ongkir;
+						?>
+						<tr>
+							<td><?php echo $no; ?></td>
+							<td><?php echo $data->nama; ?></td>
+							<td><?php echo $data->tanggal_digunakan; ?></td>
+							<td><?php echo $data->tanggal_pesan; ?></td>
+							<td><?php echo "Rp. " .number_format($totalHarga, 2, ",", "."); ?></td>
+							<td><?php echo "Rp. " .number_format($data->ongkir, 2, ",", "."); ?></td>
+							<td><?php echo $data->status; ?></td>
+						</tr>
+						<?php
+					}
+				?>
+				<tr>
+					<td colspan="4" align="right">
+						<font size="3">
+							<b>TOTAL</b>
+						</font>
+					</td>
+					<td>
+						<font size="3"><?php echo "Rp. ". number_format($totalSemua, 2, ",", "."); ?></font>
+					</td>
+					<td>
+						<font size="3">
+							<?php echo "Rp. ". number_format($totalOngkir, 2, ",", "."); ?>
+						</font>
+					</td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<?php
+header("Content-type: application/vnd-ms-excel");
+header("Content-Disposition: attachment; filename=Laporan Penjualan.xls");
+// include "data.php";
+?>
---
+++ admin/index.php
@@ -0,0 +1,58 @@
+<?php
+include "../inc/config.php"; 
+if (!empty($_SESSION['iam_admin'])) {
+    redir("home.php");
+}
+
+if (!empty($_POST)) {
+    extract($_POST);
+    $password = md5($password);
+
+    // Use prepared statements to prevent SQL injection
+    $query = "SELECT * FROM user WHERE email=? AND password=? AND status='admin'";
+    $stmt = mysqli_prepare($konek, $query);
+    
+    // Check if the prepared statement was successful
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+        mysqli_stmt_execute($stmt);
+        
+        // Get the result
+        $result = mysqli_stmt_get_result($stmt);
+
+        // Check if any rows were returned
+        if ($row = mysqli_fetch_object($result)) {
+            $_SESSION['iam_admin'] = $row->id;
+            redir("home.php");
+        } else {
+            alert("Maaf email dan password anda salah");
+        }
+
+        // Close the statement
+        mysqli_stmt_close($stmt);
+    } else {
+        // Handle the error if preparing the statement fails
+        die('Error in preparing the statement');
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel='stylesheet' href='<?php echo $url; ?>assets/bootstrap/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/style_login.css">
+</head>
+<body>
+    <div class="wrapper">
+        <form class="form-signin" action="" method="POST">       
+            <h2 class="form-signin-heading">Silahkan login</h2>
+            <input type="email" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
+            <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+        </form>
+    </div>
+</body>
+</html>
