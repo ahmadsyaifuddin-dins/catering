43a2abd9b62d78bbd7aee3ba4a26929cc0cb94b4
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-08 15:28:38 +0800

    ok

--- admin/home.php
+++ admin/home.php
@@ -1,81 +1,35 @@
-
 <?php 
 	include"../inc/config.php"; 
 	include"inc/header.php";
 	
 ?> 
-	<div class="col-md-9">
+<?php include"inc/footer.php"; 
+?>
+
+<div class="col-md-9">
 			<div class="row">
 			<div class="col-md-12">
 			<h3>Favorite Menu</h3>
-				
-				<?php 
-					$k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id ASC limit 3"); 
-					while($data = mysqli_fetch_array($k)){
-				?>
-				<div class="col-md-4 content-menu">
-					<a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
-						<img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
-						<h4><?php echo $data['nama'] ?></h4>
-					</a>
-					<p><?php echo $data['deskripsi'] ?></p>
-					<p style="font-size:18px">Harga :<?php echo number_format($data['harga'], 2, ',', '.') ?></p>
-					<p>
-						<a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>" class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
-						<a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>" class="btn btn-info btn-sm" href="#" role="button">Pesan</a>
-					</p>
-				</div>  
-				<?php } ?>
-	
-				
-			</div>
-			</div>
-			<div class="row">
-			<div class="col-md-12">
-			<hr>
-			<h3>Menu Terbaru</h3>
-				<?php 
-					$k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id DESC limit 3"); 
-					while($data = mysqli_fetch_array($k)){
-				?>
-				<div class="col-md-4 content-menu">
-					<a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
-						<img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
-						<h4><?php echo $data['nama'] ?></h4>
-					</a>
-					<p style="font-size:18px">Harga :<?php echo number_format($data['harga'], 2, ',', '.') ?></p>
-					<p>
-						<a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>" class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
-						<a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>" class="btn btn-info btn-sm" href="#" role="button">Pesan</a>
-					</p>
-				</div>  
-				<?php } ?>
-				
-			</div>
-			</div>
 
-			<div class="row">
-			<div class="col-md-12">
-			<hr>
-			<h3>Menu Satuan</h3>
-				<?php 
-					$k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id DESC limit 3"); 
-					while($data = mysqli_fetch_array($k)){
-				?>
-				<div class="col-md-4 content-menu">
-					<a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
-						<img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
-						<h4><?php echo $data['nama'] ?></h4>
-					</a>
-					<p style="font-size:18px">Harga :<?php echo number_format($data['harga'], 2, ',', '.') ?></p>
-					<p>
-						<a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>" class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
-						<a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>" class="btn btn-info btn-sm" href="#" role="button">Pesan</a>
-					</p>
-				</div>  
-				<?php } ?>
-				
-			</div>
-			</div>
-		</div>
-<?php include"inc/footer.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin</title>
+</head>
+<body>
+	
+
+
+
+
+
+
+
+
+
+</body>
+</html>
