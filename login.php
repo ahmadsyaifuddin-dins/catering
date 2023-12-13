45f313bd0ad2a8729d995814aa7e456b7e68f662
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-13 11:47:06 +0800

    Update login.php

--- login.php
+++ login.php
@@ -40,18 +40,18 @@
 			<h3>Login User</h3>
 				<br>
 				<div class="col-md-7 content-menu" style="margin-top:-20px;">
-				 
-				 <form action="" method="post" enctype="multipart/form-data">
+				
+				<form action="" method="post" enctype="multipart/form-data">
 						<label>Email</label><br>
-      <input type="email" class="form-control" name="email" placeholder="Email" required="" autofocus="" /><br>
-	  <label>Password</label><br>
-      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      <br>
+    					<input type="email" class="form-control" name="email" placeholder="Email" required="" autofocus="" /><br>
+						<label>Password</label><br>
+    					<input type="password" class="form-control" name="password" placeholder="Password" required=""/>      <br>
 						<input type="submit" name="form-input" value="Login" class="btn btn-success">
-					</form>
-				 
+				</form>
+				
 				</div>   
 				<div class="col-md-7 content-menu">
-				 Belum Punya Akun ? <a href="register.php">Buat Akun Sekarang !</a>
+				Belum Punya Akun ? <a href="register.php">Buat Akun Sekarang !</a>
 				</div>   
 					
 				
