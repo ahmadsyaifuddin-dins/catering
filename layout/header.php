87a6e5310fc318647e4427479fa2b0e7ae940e0f
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-14 15:01:25 +0800

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -179,7 +179,7 @@
 					)</a></li>
 				<?php } ?>
 			</ul>
-
+      
 			<div class="warna-head-bg" style="width:100%; height:auto; padding-top:3px; padding-bottom:3px; padding-left:10px; ">
 				<h4><font color="white">Keranjang Belanja <i class="fa-solid fa-basket-shopping"></i> </h4>
 			</div>
