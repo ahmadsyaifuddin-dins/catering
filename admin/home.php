27240abd2a34c5bf70a209e1bbdc39e019d7d71c
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-15 21:39:06 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -12,6 +12,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../assets/css/admin.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 	<title>Admin</title>
 </head>
 <body>
@@ -19,7 +20,7 @@
 <div class="container">
 			<div class="row">
 			<div class="col-sm-12">
-			<h2 class="title-head"> <i class="fa-solid fa-gear"></i> Administrator Database (Mysql)</h2>
+			<h2 class="title-head animate__hinge"> <i class="fa-solid fa-gear"></i> Administrator Database (Mysql)</h2>
 			</div>
 			</div>
 </div>
