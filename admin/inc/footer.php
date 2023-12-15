0579076d41bba46a853e3c53e338e07a17d5b4c9
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-15 21:51:37 +0800

    Update footer.php

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap_old.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
 	<title>Document</title>
 </head>
 <body>
@@ -13,7 +15,7 @@
 <div class="container" style="margin-top:20px;">
 		<footer class="text-center">
 			<font color="white">
-			<div class="col-md-12"><b> &#169; Ahmad Syaifuddin <span id="year"></span> | AS Katering Online</b>
+			<div class="col-md-12 animate__animated animate__lightSpeedInRight animate__slower"><b> &#169; Ahmad Syaifuddin <span id="year"></span> | AS Katering Online</b>
 			<!-- <br> -->
 			</div>
 			</font>
