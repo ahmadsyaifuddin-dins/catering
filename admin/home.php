8272654c0a2b7cebe56fc3fed7d552bda5805d78
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 09:04:52 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -1,32 +1,36 @@
 <?php 
 	include"../inc/config.php"; 
 	include"inc/header.php";
-?>	
+?>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../assets/css/admin.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-	<title>Admin</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <title>Admin</title>
 </head>
+
 <body>
-	
-<div class="container">
-			<div class="row">
-			<div class="col-sm-12">
-			<h2 class="title-head animate__hinge animate__delay-2s"> <i class="fa-solid fa-gear"></i> Administrator Database (Mysql)</h2>
-			</div>
-			</div>
-</div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2 class="title-head animate__hinge animate__delay-2s"> <i class="fa-solid fa-gear"></i> Administrator
+                    Database (Mysql)</h2>
+            </div>
+        </div>
+    </div>
 
 
-<?php include"inc/footer.php"; ?>
+    <?php include"inc/footer.php"; ?>
 
 </body>
+
 </html>
