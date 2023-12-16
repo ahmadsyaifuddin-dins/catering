d2577e5b42715ad386edc25690a271d442d036d7
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 22:50:53 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,24 @@
-<?php 
-	include"inc/config.php";
-	if(!empty($_SESSION['iam_user'])){
-		redir("index.php");
-	}
-	include"layout/header.php";
-	
-	
-	
-	if(!empty($_POST)){
-		extract($_POST);
-		$password = md5($password);
-		$q = mysqli_query($konek, "SELECT * FROM `user` WHERE email='$email' AND password='$password' AND status='user'")or die(mysqli_error());
-		if($q){
-			$r = mysqli_fetch_object($q);
-			$_SESSION['iam_user'] = $r->id;
-			redir("index.php");
-		}else{
-			alert("Maaf email dan password anda salah");
-		}
-	}
+<?php
+include "inc/config.php";
+if (!empty($_SESSION['iam_user'])) {
+    redir("index.php");
+}
+include "layout/header.php";
+
+
+
+if (!empty($_POST)) {
+    extract($_POST);
+    $password = md5($password);
+    $q = mysqli_query($konek, "SELECT * FROM `user` WHERE email='$email' AND password='$password' AND status='user'") or die(mysqli_error());
+    if ($q) {
+        $r = mysqli_fetch_object($q);
+        $_SESSION['iam_user'] = $r->id;
+        redir("index.php");
+    } else {
+        alert("Maaf email dan password anda salah");
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,29 +34,28 @@
             <div class="row">
                 <div class="col-md-12">
 
-                    <?php 
-				if(!empty($_POST)){
-			extract($_POST); 
-		
-			$q = mysqli_query($konek, "INSERT INTO kontak VALUES(NULL,'$nama','$email','$subjek','$pesan')");
-				if($q){  
-			?>
+                    <?php
+                    if (!empty($_POST)) {
+                        extract($_POST);
 
-                    <div class="alert alert-success">Terimakasih atas masukannya</div>
-                    <?php }else{ ?>
-                    <div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
-                    <?php } } ?>
+                        $q = mysqli_query($konek, "INSERT INTO kontak VALUES(NULL,'$nama','$email','$subjek','$pesan')");
+                        if ($q) {
+                    ?>
+
+                            <div class="alert alert-success">Terimakasih atas masukannya</div>
+                        <?php } else { ?>
+                            <div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
+                    <?php }
+                    } ?>
                     <h3>Login User</h3>
                     <br>
                     <div class="col-md-7 content-menu" style="margin-top:-20px;">
 
                         <form action="" method="post" enctype="multipart/form-data">
                             <label>Email</label><br>
-                            <input type="email" class="form-control" name="email" placeholder="Email" required=""
-                                autofocus="" /><br>
+                            <input type="email" class="form-control" name="email" placeholder="Email" required="" autofocus="" /><br>
                             <label>Password</label><br>
-                            <input type="password" class="form-control" name="password" placeholder="Password"
-                                required="" /> <br>
+                            <input type="password" class="form-control" name="password" placeholder="Password" required="" /> <br>
                             <input type="submit" name="form-input" value="Login" class="btn btn-success">
                         </form>
 
@@ -73,4 +72,4 @@
 
 </html>
 
-<?php include"layout/footer.php"; ?>
+<?php include "layout/footer.php"; ?>
