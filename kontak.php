26bae7902ad550a4e8482c3d0083b64c6093406e
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 22:50:13 +0800

    Update kontak.php

--- kontak.php
+++ kontak.php
@@ -1,6 +1,6 @@
-<?php 
-	include"inc/config.php";
-	include"layout/header.php";	
+<?php
+include "inc/config.php";
+include "layout/header.php";
 ?>
 
 <!DOCTYPE html>
@@ -18,32 +18,33 @@
         <div class="row">
             <div class="col-md-12">
 
-                <?php 
-				if(!empty($_POST)){
-			extract($_POST); 
-		
-			$q = mysqli_query($konek, "INSERT INTO kontak VALUES(NULL,'$nama','$email','$subjek','$pesan')");
-				if($q){  
-			?>
-                <font color="black">
-                    <!-- <div class="alert alert-success">Terimakasih atas masukannya</div> -->
-                    <script type='text/javascript'>
-                    setTimeout(function() {
-                        Swal.fire({
-                            title: "Pesan Terkirim!",
-                            text: "Terima kasih Atas Masukannya",
-                            icon: "success",
-                            timer: "3000",
-                            showConfirmButton: true
-                        });
-                    }, 1);
-                    window.setTimeout(function() {
-                        window.location.replace('kontak.php');
-                    }, 3000);
-                    </script>
-                    <?php }else{ ?>
-                    <div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
-                    <?php } } ?>
+                <?php
+                if (!empty($_POST)) {
+                    extract($_POST);
+
+                    $q = mysqli_query($konek, "INSERT INTO kontak VALUES(NULL,'$nama','$email','$subjek','$pesan')");
+                    if ($q) {
+                ?>
+                        <font color="black">
+                            <!-- <div class="alert alert-success">Terimakasih atas masukannya</div> -->
+                            <script type='text/javascript'>
+                                setTimeout(function() {
+                                    Swal.fire({
+                                        title: "Pesan Terkirim!",
+                                        text: "Terima kasih Atas Masukannya",
+                                        icon: "success",
+                                        timer: "3000",
+                                        showConfirmButton: true
+                                    });
+                                }, 1);
+                                window.setTimeout(function() {
+                                    window.location.replace('kontak.php');
+                                }, 3000);
+                            </script>
+                        <?php } else { ?>
+                            <div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
+                    <?php }
+                } ?>
                     <h3>
                         <font color="black">Kontak Kami
                     </h3>
@@ -60,8 +61,7 @@
                                 <input type="text" class="form-control" name="subjek" required><br>
                                 <label>Pesan</label><br>
                                 <textarea class="form-control" name="pesan" required></textarea><br>
-                                <input type="submit" name="form-input" id="sweetAlert" value="Simpan"
-                                    class="btn btn-success">
+                                <input type="submit" name="form-input" id="sweetAlert" value="Simpan" class="btn btn-success">
                         </form>
 
                     </div>
@@ -77,26 +77,26 @@
 
 </html>
 
-<?php include"layout/footer.php"; ?>
+<?php include "layout/footer.php"; ?>
 
 
 <script>
-// 	$(document).ready(function () {
-// 		$("#sweetAlert").click(function (e) {
-// 			e.preventDefault();
-// 			Swal.fire({
-// 			title: "Terkirim!",
-// 			text: "Terima kasih Atas Masukannya",
-// 			icon: "success"
-// });
-// 		})
-// 	})
+    // 	$(document).ready(function () {
+    // 		$("#sweetAlert").click(function (e) {
+    // 			e.preventDefault();
+    // 			Swal.fire({
+    // 			title: "Terkirim!",
+    // 			text: "Terima kasih Atas Masukannya",
+    // 			icon: "success"
+    // });
+    // 		})
+    // 	})
 
-// $(document).on('click','#sweetAlert', function() {
-// 	Swal.fire(
-// 		'Sukses',
-// 		'Terima Kasih Telah beri masukan',
-// 		'success'
-// 	)
-// })
+    // $(document).on('click','#sweetAlert', function() {
+    // 	Swal.fire(
+    // 		'Sukses',
+    // 		'Terima Kasih Telah beri masukan',
+    // 		'success'
+    // 	)
+    // })
 </script>
