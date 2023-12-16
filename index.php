c92e96accb0dd27c6efb06867e309584f38d8089
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 22:29:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php 
-	include"inc/config.php";
-	include"layout/header.php";
-	
+<?php
+include "inc/config.php";
+include "layout/header.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -23,29 +23,27 @@
                     <h2 class="animate__animated animate__lightSpeedInLeft animate__slower" align="center"> <b>Favorite
                             Menu</b> </h2>
 
-                    <?php 
-					$k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id ASC limit 3"); 
-					while($data = mysqli_fetch_array($k)){
-				?>
-                    <div class="col-md-4">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
-                                    <img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
-                                    <h4><?php echo $data['nama'] ?></h4>
-                                </a>
-                                <p><?php echo $data['deskripsi'] ?></p>
-                                <p style="font-size:18px">Harga : <strong> Rp
-                                        <?php echo number_format($data['harga'], 0, ',', '.') ?></strong> </p>
-                                <p>
-                                    <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>"
-                                        class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
-                                    <a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>"
-                                        class="btn btn-warning btn-sm" href="#" role="button">Pesan</a>
-                                </p>
+                    <?php
+                    $k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id ASC limit 3");
+                    while ($data = mysqli_fetch_array($k)) {
+                    ?>
+                        <div class="col-md-4">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
+                                        <img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
+                                        <h4><?php echo $data['nama'] ?></h4>
+                                    </a>
+                                    <p><?php echo $data['deskripsi'] ?></p>
+                                    <p style="font-size:18px">Harga : <strong> Rp
+                                            <?php echo number_format($data['harga'], 0, ',', '.') ?></strong> </p>
+                                    <p>
+                                        <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>" class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
+                                        <a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>" class="btn btn-warning btn-sm" href="#" role="button">Pesan</a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     <?php } ?>
 
                 </div>
@@ -54,28 +52,26 @@
                 <div class="col-md-13">
                     <h2 class="animate__animated animate__lightSpeedInLeft animate__slower" align="center"> <b>Menu
                             Terbaru</b> </h2>
-                    <?php 
-					$k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id DESC limit 3"); 
-					while($data = mysqli_fetch_array($k)){
-				?>
-                    <div class="col-md-4">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
-                                    <img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
-                                    <h4><?php echo $data['nama'] ?></h4>
-                                </a>
-                                <p style="font-size:18px">Harga : <strong>Rp
-                                        <?php echo number_format($data['harga'], 0, ',', '.') ?></strong> </p>
-                                <p>
-                                    <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>"
-                                        class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
-                                    <a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>"
-                                        class="btn btn-warning btn-sm" href="#" role="button">Pesan</a>
-                                </p>
+                    <?php
+                    $k = mysqli_query($konek, "SELECT * FROM produk ORDER BY id DESC limit 3");
+                    while ($data = mysqli_fetch_array($k)) {
+                    ?>
+                        <div class="col-md-4">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>">
+                                        <img src="<?php echo $url; ?>uploads/<?php echo $data['gambar'] ?>" width="100%">
+                                        <h4><?php echo $data['nama'] ?></h4>
+                                    </a>
+                                    <p style="font-size:18px">Harga : <strong>Rp
+                                            <?php echo number_format($data['harga'], 0, ',', '.') ?></strong> </p>
+                                    <p>
+                                        <a href="<?php echo $url; ?>menu.php?id=<?php echo $data['id'] ?>" class="btn btn-success btn-sm" href="#" role="button">Lihat Detail</a>
+                                        <a href="<?php echo $url; ?>keranjang.php?act=beli&&produk_id=<?php echo $data['id'] ?>" class="btn btn-warning btn-sm" href="#" role="button">Pesan</a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     <?php } ?>
 
                 </div>
@@ -86,4 +82,4 @@
 </html>
 
 
-<?php include"layout/footer.php"; ?>
+<?php include "layout/footer.php"; ?>
