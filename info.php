25c73ef06777644a331ad3416abac0a7fa0df7ad
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 09:18:06 +0800

    Update info.php

--- info.php
+++ info.php
@@ -2,15 +2,28 @@
 	include"inc/config.php"; 
 	include"layout/header.php";	
 ?>
-<div class="col-md-9">
-    <div class="row">
-        <?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="col-md-9">
+        <div class="row">
+            <?php
 			$q = mysqli_query($konek, "Select * from info_pembayaran limit 1") or die (mysqli_error());
 			$data = mysqli_fetch_object($q);
 		?>
-        <pre><?php echo $data->info; ?></pre>
+            <pre><?php echo $data->info; ?></pre>
+        </div>
     </div>
-</div>
+</body>
+
+</html>
+
 <?php
 	include "layout/footer.php";
 ?>
