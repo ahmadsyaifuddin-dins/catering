be8de5d6b0e3e21460f6415de00c6c8ab80c75ff
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 22:50:37 +0800

    Update lihatkontak.php

--- lihatkontak.php
+++ lihatkontak.php
@@ -1,6 +1,6 @@
-<?php 
-	include"inc/config.php";
-	include"layout/header.php";	
+<?php
+include "inc/config.php";
+include "layout/header.php";
 ?>
 
 <!DOCTYPE html>
@@ -16,22 +16,23 @@
     <div class="col-md-9">
         <div class="row">
             <div class="col-md-12">
-                <?php 
-				if(!empty($_POST)){
-				extract($_POST); 
-				$qs = mysqli_query($konek, "INSERT INTO kontak VALUES(NULL,'$nama','$email','$subjek','$pesan')");
-					if($qs){  
-			?>
-                <font color="black">
-                    <div class="alert alert-success">Terimakasih atas masukannya</div>
-                    <?php }else{ ?>
-                    <div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
-                    <?php } } ?>
+                <?php
+                if (!empty($_POST)) {
+                    extract($_POST);
+                    $qs = mysqli_query($konek, "INSERT INTO kontak VALUES(NULL,'$nama','$email','$subjek','$pesan')");
+                    if ($qs) {
+                ?>
+                        <font color="black">
+                            <div class="alert alert-success">Terimakasih atas masukannya</div>
+                        <?php } else { ?>
+                            <div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
+                    <?php }
+                } ?>
                     <?php
-				$q = mysqli_query($konek, "select*from kontak");
-				$j = mysqli_num_rows($q);
-			?>
-                    <h4>Daftar Kontak Masuk (<?php echo ($j>0)?$j:0; ?>)</h4>
+                    $q = mysqli_query($konek, "select*from kontak");
+                    $j = mysqli_num_rows($q);
+                    ?>
+                    <h4>Daftar Kontak Masuk (<?php echo ($j > 0) ? $j : 0; ?>)</h4>
                     <table class="table table-striped table-hove">
                         <thead style="color:black;">
                             <tr>
@@ -45,14 +46,14 @@
                         </thead>
                         <tbody>
 
-                            <?php while($data=mysqli_fetch_object($q)){ ?>
-                            <tr>
-                                <th scope="row" style="color:black;"><?php echo $no++; ?></th>
-                                <td style="color:black;"><?php echo $data->nama ?></td>
-                                <td style="color:black;"><?php echo $data->email ?></td>
-                                <td style="color:black;"><?php echo $data->subjek ?></td>
-                                <td style="color:black;"><?php echo $data->pesan ?></td>
-                            </tr>
+                            <?php while ($data = mysqli_fetch_object($q)) { ?>
+                                <tr>
+                                    <th scope="row" style="color:black;"><?php echo $no++; ?></th>
+                                    <td style="color:black;"><?php echo $data->nama ?></td>
+                                    <td style="color:black;"><?php echo $data->email ?></td>
+                                    <td style="color:black;"><?php echo $data->subjek ?></td>
+                                    <td style="color:black;"><?php echo $data->pesan ?></td>
+                                </tr>
                             <?php } ?>
                         </tbody>
                     </table>
@@ -64,4 +65,4 @@
 
 </html>
 
-<?php include"layout/footer.php"; ?>
+<?php include "layout/footer.php"; ?>
