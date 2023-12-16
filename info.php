242a98cd477d24cec882653d415bfbbf452f897f
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 09:08:01 +0800

    memperbaiki penulisan code lebih rapi

--- info.php
+++ info.php
@@ -3,13 +3,13 @@
 	include"layout/header.php";	
 ?>
 <div class="col-md-9">
-	<div class="row">
-		<?php
+    <div class="row">
+        <?php
 			$q = mysqli_query($konek, "Select * from info_pembayaran limit 1") or die (mysqli_error());
 			$data = mysqli_fetch_object($q);
 		?>
-		<pre><?php echo $data->info; ?></pre>
-	</div>
+        <pre><?php echo $data->info; ?></pre>
+    </div>
 </div>
 <?php
 	include "layout/footer.php";
