4f92e35cfa8c35fa208cad90514611e69f5d374a
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-16 22:56:29 +0800

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,35 +1,37 @@
 <?php
-	session_start();
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "catering";
-	
-	// Create connection
-	$konek = mysqli_connect($servername, $username, $password, $database);
-	
-	
-	// Check connection
-	if ($konek->connect_error) {
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "catering";
+
+// Create connection
+$konek = mysqli_connect($servername, $username, $password, $database);
+
+
+// Check connection
+if ($konek->connect_error) {
 	die("Connection failed: " . $konek->connect_error);
-	}
-	
-	// settings
-	$url = "http://localhost/catering/";
-	$title = "Asai's Kitchen";
-	$no = 1;
+}
 
-	function alert($command){
-		echo "<script>alert('".$command."');</script>";
-	}
+// settings
+$url = "http://localhost/catering/";
+$title = "Asai's Kitchen";
+$no = 1;
 
-	function redir($command){
-		echo "<script>document.location='".$command."';</script>";
-	}
+function alert($command)
+{
+	echo "<script>alert('" . $command . "');</script>";
+}
 
-	function validate_admin_not_login($command){
-		if(empty($_SESSION['iam_admin'])){
-			redir($command);
-		}
+function redir($command)
+{
+	echo "<script>document.location='" . $command . "';</script>";
+}
+
+function validate_admin_not_login($command)
+{
+	if (empty($_SESSION['iam_admin'])) {
+		redir($command);
 	}
-?>
+}
