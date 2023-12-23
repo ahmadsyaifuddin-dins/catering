e27d179e42c3450620deb24cbcc7312d64b331af
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-23 10:15:21 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -44,30 +44,30 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 ?>
 
 <?php if (!isset($_POST['password'])) : ?>
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Login Form</title>
-    <link rel='stylesheet' href='<?php echo $url; ?>assets/bootstrap/css/bootstrap.min.css'>
-    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/style_login.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.min.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Form</title>
+        <link rel='stylesheet' href='<?php echo $url; ?>assets/bootstrap/css/bootstrap.min.css'>
+        <link rel="stylesheet" href="<?php echo $url; ?>assets/css/style_login.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.min.css">
+    </head>
 
-<body>
-    <div class="wrapper">
-        <form class="form-signin" action="" method="POST">
-            <h2 class="form-signin-heading">Silahkan login</h2>
-            <input type="email" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
-            <input type="password" class="form-control" name="password" placeholder="Password" required="" />
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-        </form>
-    </div>
+    <body>
+        <div class="wrapper">
+            <form class="form-signin" action="" method="POST">
+                <h2 class="form-signin-heading">Silahkan login</h2>
+                <input type="email" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
+                <input type="password" class="form-control" name="password" placeholder="Password" required="" />
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+            </form>
+        </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js"></script>
+    </body>
 
-</html>
+    </html>
 <?php endif; ?>
