0790d2d29ccd7081720312f47a89a2a38421c91b
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-23 10:59:19 +0800

    ok

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap_old.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+	<link rel="stylesheet" href="../../assets/css/admin.css">
 
 	<title>Document</title>
 </head>
@@ -14,7 +15,7 @@
 <body>
 
 
-	<div class="container" style="margin-top:33%;">
+	<div class="container" style="margin-top:32%;">
 		<footer class="text-center">
 			<font color="white">
 				<div class="col-md-12 animate__animated animate__lightSpeedInLeft animate__slower"><b> &#169; Ahmad
