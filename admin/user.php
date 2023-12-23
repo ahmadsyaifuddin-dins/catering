25dbfe1156ef248487301435f0469812dfaa47f1
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-23 11:10:27 +0800

    ok

--- admin/user.php
+++ admin/user.php
@@ -30,109 +30,115 @@
 	
 	include"inc/header.php";
 	
-?> 
-	<link rel="stylesheet" href="../assets/css/border.css">
+?>
+<link rel="stylesheet" href="../assets/css/border.css">
 
-	<div class="container">
-		<?php
+<div class="container">
+    <?php
 			$q = mysqli_query($konek, "select*from user");
 			$j = mysqli_num_rows($q);
 		?>
-		<h4>Daftar User Masuk (<?php echo ($j>0)?$j:0; ?>)</h4>
-		<a class="btn btn-sm btn-primary" href="user.php?act=create">Tambah Data User <i class="fa-solid fa-user-plus"></i> </a>
-		<hr>
-		<?php
+    <h4>Daftar User Masuk (<?php echo ($j>0)?$j:0; ?>)</h4>
+    <a class="btn btn-sm btn-primary" href="user.php?act=create">Tambah Data User <i class="fa-solid fa-user-plus"></i>
+    </a>
+    <hr>
+    <?php
 			if(!empty($_GET)){
 				if($_GET['act'] == 'create'){
 				?>
-					<div class="row col-md-6">
-					<form action="" method="post" enctype="multipart/form-data">
-						<label>Nama</label><br>
-						<input type="text" class="form-control" name="nama" required><br>
-						<label>Email</label><br>
-						<input type="email" class="form-control" name="email" required><br>
-						<label>Telephone</label><br>
-						<input type="text" class="form-control" name="telephone" required><br>
-						<label>Alamat</label><br>
-						<input type="text" class="form-control" name="alamat" required><br>
-						<label>Password</label><br>
-						<input type="password" class="form-control" name="password" required><br>
-						<label>Status</label><br> 
-						
-						<select name="status" required class="form-control"> 
-							<option value="user">User</option> 
-							<option value="admin">Admin</option> 
-						</select><br>
-						<input type="submit" name="form-input" value="Simpan" class="btn btn-success">
-					</form>
-					</div>
-					<div class="row col-md-12"><hr></div>
-				<?php	
+    <div class="row col-md-6">
+        <form action="" method="post" enctype="multipart/form-data">
+            <label>Nama</label><br>
+            <input type="text" class="form-control" name="nama" required><br>
+            <label>Email</label><br>
+            <input type="email" class="form-control" name="email" required><br>
+            <label>Telephone</label><br>
+            <input type="text" class="form-control" name="telephone" required><br>
+            <label>Alamat</label><br>
+            <input type="text" class="form-control" name="alamat" required><br>
+            <label>Password</label><br>
+            <input type="password" class="form-control" name="password" required><br>
+            <label>Status</label><br>
+
+            <select name="status" required class="form-control">
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+            </select><br>
+            <input type="submit" name="form-input" value="Simpan" class="btn btn-success">
+        </form>
+    </div>
+    <div class="row col-md-12">
+        <hr>
+    </div>
+    <?php	
 				} 
 				if($_GET['act'] == 'edit'){
 					$data = mysqli_fetch_object(mysqli_query($konek, "select*from user where id='$_GET[id]'"));
 				?>
-					<div class="row col-md-6">
-					<form action="user.php?act=edit&&id=<?php echo $_GET['id'] ?>" method="post" enctype="multipart/form-data">
-						<label>Nama</label><br>
-						<input type="text" class="form-control" name="nama" value="<?php echo $data->nama; ?>" required><br>
-						<label>Email</label><br>
-						<input type="email" class="form-control" name="email" value="<?php echo $data->email; ?>" required><br>
-						<label>Telephone</label><br>
-						<input type="text" class="form-control" name="telephone" value="<?php echo $data->telephone; ?>" required><br>
-						<label>Alamat</label><br>
-						<input type="text" class="form-control" name="alamat" value="<?php echo $data->alamat; ?>" required><br>
-						<label>Password</label><br>
-						<input type="password" class="form-control" name="password" required><br>
-						<label>Status</label><br>
-						<select name="status" required class="form-control"> 
-						 
-								<option value="<?php echo $data->status; ?>"><?php echo $data->status; ?></option>
-								<option value="user">User</option> 
-								<option value="admin">Admin</option> 
-						</select><br>
-						<input type="submit" name="form-edit" value="Simpan" class="btn btn-success">
-					</form>
-					</div>
-					<div class="row col-md-12"><hr></div>
-				<?php	
+    <div class="row col-md-6">
+        <form action="user.php?act=edit&&id=<?php echo $_GET['id'] ?>" method="post" enctype="multipart/form-data">
+            <label>Nama</label><br>
+            <input type="text" class="form-control" name="nama" value="<?php echo $data->nama; ?>" required><br>
+            <label>Email</label><br>
+            <input type="email" class="form-control" name="email" value="<?php echo $data->email; ?>" required><br>
+            <label>Telephone</label><br>
+            <input type="text" class="form-control" name="telephone" value="<?php echo $data->telephone; ?>"
+                required><br>
+            <label>Alamat</label><br>
+            <input type="text" class="form-control" name="alamat" value="<?php echo $data->alamat; ?>" required><br>
+            <label>Password</label><br>
+            <input type="password" class="form-control" name="password" required><br>
+            <label>Status</label><br>
+            <select name="status" required class="form-control">
+
+                <option value="<?php echo $data->status; ?>"><?php echo $data->status; ?></option>
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+            </select><br>
+            <input type="submit" name="form-edit" value="Simpan" class="btn btn-success">
+        </form>
+    </div>
+    <div class="row col-md-12">
+        <hr>
+    </div>
+    <?php	
 				} 
 			}
 		?>
-		
-		<table class="table"> 
-			<thead style="background:#00b4d8"> 
-				<tr> 
-					<th>No</th> 
-					<th>Nama User</th> 
-					<th>Email User</th> 
-					<th>Telephone User</th> 
-					<th>Alamat User</th> 
-					<th>Status</th> 
-					<th>Aksi</th> 
-				</tr> 
-			</thead> 
-			<tbody> 
-				
 
-			
-			
-		<?php while($data=mysqli_fetch_object($q)){ ?> 
-				<tr> 
-					<th scope="row"><?php echo $no++; ?></th> 
-					<td><?php echo $data->nama ?></td> 
-					<td><?php echo $data->email ?></td> 
-					<td><?php echo $data->telephone ?></td> 
-					<td><?php echo $data->alamat ?></td> 
-					<td><?php echo $data->status ?></td> 
-					<td>
-						<a class="btn btn-sm btn-success" href="user.php?act=edit&&id=<?php echo $data->id ?>">Edit <i class="fa-solid fa-user-pen"></i> </a>
-						<a class="btn btn-sm btn-danger" href="user.php?act=delete&&id=<?php echo $data->id ?>">Delete <i class="fa-solid fa-trash"></i> </a>
-					</td> 
-				</tr>
-		<?php } ?>
-			</tbody> 
-		</table> 
-    </div> <!-- /container -->
-	
-<?php include"inc/footer.php"; ?>
+    <table class="table">
+        <thead style="background:#00b4d8">
+            <tr>
+                <th>No</th>
+                <th>Nama User</th>
+                <th>Email User</th>
+                <th>Telephone User</th>
+                <th>Alamat User</th>
+                <th>Status</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+
+
+
+
+            <?php while($data=mysqli_fetch_object($q)){ ?>
+            <tr>
+                <th scope="row"><?php echo $no++; ?></th>
+                <td><?php echo $data->nama ?></td>
+                <td><?php echo $data->email ?></td>
+                <td><?php echo $data->telephone ?></td>
+                <td><?php echo $data->alamat ?></td>
+                <td><?php echo $data->status ?></td>
+                <td>
+                    <a class="btn btn-sm btn-success" href="user.php?act=edit&&id=<?php echo $data->id ?>">Edit <i
+                            class="fa-solid fa-user-pen"></i> </a>
+                    <a class="btn btn-sm btn-danger" href="user.php?act=delete&&id=<?php echo $data->id ?>">Delete <i
+                            class="fa-solid fa-trash"></i> </a>
+                </td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div> <!-- /container -->
