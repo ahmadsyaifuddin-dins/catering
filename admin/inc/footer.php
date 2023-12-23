53802b41caa7fffdecc105f942b6cb78d073f587
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-23 11:42:30 +0800

    sip testing pembayaran success

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -2,45 +2,43 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap_old.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-	<link rel="stylesheet" href="../../assets/css/admin.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap_old.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="../../assets/css/admin.css">
 
-	<title>Document</title>
+    <title>Document</title>
 </head>
 
 <body>
 
 
-	<div class="container" style="margin-top:32%;">
-		<footer class="text-center">
-			<font color="white">
-				<div class="col-md-12 animate__animated animate__lightSpeedInLeft animate__slower"><b> &#169; Ahmad
-						Syaifuddin <span id="year"></span> | AS Katering Online</b>
-					<!-- <br> -->
-				</div>
-			</font>
-		</footer>
-	</div>
-	<script src="<?php echo $url ?>assets/js/jquery.js"></script>
-	<script src="<?php echo $url ?>assets/bootstrap/js/bootstrap.min.js"></script>
-	<script src="<?php echo $url ?>assets/bootstrap/js/moment.js"></script>
-	<script src="<?php echo $url ?>assets/bootstrap/js/bootstrap-datetimepicker.min.js"></script>
-	<script type="text/javascript">
-		$(function() {
-			$('#datetimepicker').datetimepicker({
-				format: 'YYYY-MM-DD',
-			});
-			$('#datetimepicker2').datetimepicker({
-				format: 'YYYY-MM-DD',
-			});
-		});
+    <div class="container" style="margin-top:32%;">
+        <footer class="text-center">
+            <font color="white">
+                <div class="col-md-12 animate__animated animate__lightSpeedInLeft animate__slower"><b> &#169; Ahmad
+                        Syaifuddin <span id="year"></span> | AS Katering Online</b>
+                    <!-- <br> -->
+                </div>
+            </font>
+        </footer>
+    </div>
+    <script src="<?php echo $url ?>assets/bootstrap/js/moment.js"></script>
+    <script src="<?php echo $url ?>assets/bootstrap/js/bootstrap-datetimepicker.min.js"></script>
+    <script type="text/javascript">
+    $(function() {
+        $('#datetimepicker').datetimepicker({
+            format: 'YYYY-MM-DD',
+        });
+        $('#datetimepicker2').datetimepicker({
+            format: 'YYYY-MM-DD',
+        });
+    });
 
-		document.getElementById("year").innerHTML = new Date().getFullYear();
-	</script>
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+    </script>
 </body>
 
 </html>
--- admin/produk.php
+++ admin/produk.php
@@ -4,46 +4,46 @@ include "../inc/config.php";
 
 
 if (!empty($_GET)) {
-	if ($_GET['act'] == 'delete') {
+    if ($_GET['act'] == 'delete') {
 
-		$q = mysqli_query($konek, "delete from produk WHERE id='$_GET[id]'");
-		if ($q) {
-			alert("Success");
-			redir("produk.php");
-		}
-	}
+        $q = mysqli_query($konek, "delete from produk WHERE id='$_GET[id]'");
+        if ($q) {
+            alert("Success");
+            redir("produk.php");
+        }
+    }
 }
 if (!empty($_GET['act']) && $_GET['act'] == 'create') {
-	if (!empty($_POST)) {
-		$gambar = md5('Y-m-d H:i:s') . $_FILES['gambar']['name'];
-		extract($_POST);
-		$deskripsi = (!empty($_POST['deskripsi'])) ? $_POST['deskripsi'] : NULL;
-		$q = mysqli_query($konek, "insert into produk Values(NULL,'$nama','$deskripsi','$gambar','$harga','$kategori_produk_id')");
-		if ($q) {
-			$upload = move_uploaded_file($_FILES['gambar']['tmp_name'], '../uploads/' . $gambar);
-			if ($upload) {
-				alert("Success");
-				redir("produk.php");
-			}
-		}
-	}
+    if (!empty($_POST)) {
+        $gambar = md5('Y-m-d H:i:s') . $_FILES['gambar']['name'];
+        extract($_POST);
+        $deskripsi = (!empty($_POST['deskripsi'])) ? $_POST['deskripsi'] : NULL;
+        $q = mysqli_query($konek, "insert into produk Values(NULL,'$nama','$deskripsi','$gambar','$harga','$kategori_produk_id')");
+        if ($q) {
+            $upload = move_uploaded_file($_FILES['gambar']['tmp_name'], '../uploads/' . $gambar);
+            if ($upload) {
+                alert("Success");
+                redir("produk.php");
+            }
+        }
+    }
 }
 if (!empty($_GET['act']) && $_GET['act'] == 'edit') {
-	if (!empty($_POST)) {
+    if (!empty($_POST)) {
 
-		$gambar = md5('Y-m-d H:i:s') . $_FILES['gambar']['name'];
-		extract($_POST);
-		$deskripsi = (!empty($_POST['deskripsi'])) ? $_POST['deskripsi'] : NULL;
+        $gambar = md5('Y-m-d H:i:s') . $_FILES['gambar']['name'];
+        extract($_POST);
+        $deskripsi = (!empty($_POST['deskripsi'])) ? $_POST['deskripsi'] : NULL;
 
-		$q = mysqli_query($konek, "update produk SET nama='$nama',deskripsi='$deskripsi', gambar='$gambar', harga='$harga', kategori_produk_id='$kategori_produk_id' where id=$_GET[id]") or die(mysqli_error());
-		if ($q) {
-			$upload = move_uploaded_file($_FILES['gambar']['tmp_name'], '../uploads/' . $gambar);
-			if ($upload) {
-				alert("Success");
-				redir("produk.php");
-			}
-		}
-	}
+        $q = mysqli_query($konek, "update produk SET nama='$nama',deskripsi='$deskripsi', gambar='$gambar', harga='$harga', kategori_produk_id='$kategori_produk_id' where id=$_GET[id]") or die(mysqli_error());
+        if ($q) {
+            $upload = move_uploaded_file($_FILES['gambar']['tmp_name'], '../uploads/' . $gambar);
+            if ($upload) {
+                alert("Success");
+                redir("produk.php");
+            }
+        }
+    }
 }
 
 
@@ -54,81 +54,81 @@ include "inc/header.php";
 
 <div class="container">
     <?php
-	$q = mysqli_query($konek, "select*from produk");
-	$j = mysqli_num_rows($q);
-	?>
+    $q = mysqli_query($konek, "select*from produk");
+    $j = mysqli_num_rows($q);
+    ?>
     <h4>Daftar Produk (<?php echo ($j > 0) ? $j : 0; ?>)</h4>
     <a class="btn btn-sm btn-primary" href="produk.php?act=create"> <i class="fa-solid fa-plus"></i> Tambah Produk </a>
     <hr>
     <?php
-	if (!empty($_GET)) {
-		if ($_GET['act'] == 'create') {
-	?>
-    <div class="row col-md-6">
-        <form action="" method="post" enctype="multipart/form-data">
-            <label>Kategori Produk</label><br>
-            <select name="kategori_produk_id" required class="form-control">
-                <?php
-						$katpro = mysqli_query($konek, "select*from kategori_produk");
-						while ($kp = mysqli_fetch_array($katpro)) {
-						?>
-                <option value="<?php echo $kp['id']; ?>"><?php echo $kp['nama'] ?></option>
-                <?php } ?>
-            </select><br>
-            <label>Nama</label><br>
-            <input type="text" class="form-control" name="nama" required><br>
-            <label>Deskripsi</label><br>
-            <textarea class="form-control" name="deskripsi" required></textarea><br>
-            <label>Gambar</label><br>
-            <input type="file" class="form-control" name="gambar" required><br>
-            <label>Harga</label><br>
-            <input type="number" class="form-control" name="harga" required><br>
-            <input type="submit" name="form-input" value="Simpan" class="btn btn-success">
-        </form>
-    </div>
-    <div class="row col-md-12">
-        <hr>
-    </div>
+    if (!empty($_GET)) {
+        if ($_GET['act'] == 'create') {
+    ?>
+            <div class="row col-md-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <label>Kategori Produk</label><br>
+                    <select name="kategori_produk_id" required class="form-control">
+                        <?php
+                        $katpro = mysqli_query($konek, "select*from kategori_produk");
+                        while ($kp = mysqli_fetch_array($katpro)) {
+                        ?>
+                            <option value="<?php echo $kp['id']; ?>"><?php echo $kp['nama'] ?></option>
+                        <?php } ?>
+                    </select><br>
+                    <label>Nama</label><br>
+                    <input type="text" class="form-control" name="nama" required><br>
+                    <label>Deskripsi</label><br>
+                    <textarea class="form-control" name="deskripsi" required></textarea><br>
+                    <label>Gambar</label><br>
+                    <input type="file" class="form-control" name="gambar" required><br>
+                    <label>Harga</label><br>
+                    <input type="number" class="form-control" name="harga" required><br>
+                    <input type="submit" name="form-input" value="Simpan" class="btn btn-success">
+                </form>
+            </div>
+            <div class="row col-md-12">
+                <hr>
+            </div>
+        <?php
+        }
+        if ($_GET['act'] == 'edit') {
+            $data = mysqli_fetch_object(mysqli_query($konek, "select*from produk where id='$_GET[id]'"));
+        ?>
+            <div class="row col-md-6">
+                <form action="produk.php?act=edit&&id=<?php echo $_GET['id'] ?>" method="post" enctype="multipart/form-data">
+                    <label>Kategori Produk</label><br>
+                    <select name="kategori_produk_id" required class="form-control">
+                        <?php
+                        $katpro = mysqli_query($konek, "select*from kategori_produk where id='$data->kategori_produk_id'");
+                        $kpa = mysqli_fetch_array($katpro);
+                        ?>
+                        <option value="<?php echo $kpa['id']; ?>"><?php echo $kpa['nama'] ?></option>
+                        <?php
+                        $katpro = mysqli_query($konek, "select*from kategori_produk");
+                        while ($kp = mysqli_fetch_array($katpro)) {
+                        ?>
+                            <option value="<?php echo $kp['id']; ?>"><?php echo $kp['nama'] ?></option>
+                        <?php } ?>
+                    </select><br>
+                    <br>
+                    <label>Nama</label><br>
+                    <input type="text" class="form-control" name="nama" value="<?php echo $data->nama; ?>"><br>
+                    <label>Deskripsi</label><br>
+                    <textarea class="form-control" name="deskripsi" required><?php echo $data->deskripsi; ?></textarea><br>
+                    <label>Gambar</label><br>
+                    <input type="file" class="form-control" name="gambar" required><br>
+                    <label>Harga</label><br>
+                    <input type="number" class="form-control" name="harga" required value="<?php echo $data->harga; ?>"><br>
+                    <input type="submit" name="form-edit" value="Simpan" class="btn btn-success">
+                </form>
+            </div>
+            <div class="row col-md-12">
+                <hr>
+            </div>
     <?php
-		}
-		if ($_GET['act'] == 'edit') {
-			$data = mysqli_fetch_object(mysqli_query($konek, "select*from produk where id='$_GET[id]'"));
-		?>
-    <div class="row col-md-6">
-        <form action="produk.php?act=edit&&id=<?php echo $_GET['id'] ?>" method="post" enctype="multipart/form-data">
-            <label>Kategori Produk</label><br>
-            <select name="kategori_produk_id" required class="form-control">
-                <?php
-						$katpro = mysqli_query($konek, "select*from kategori_produk where id='$data->kategori_produk_id'");
-						$kpa = mysqli_fetch_array($katpro);
-						?>
-                <option value="<?php echo $kpa['id']; ?>"><?php echo $kpa['nama'] ?></option>
-                <?php
-						$katpro = mysqli_query($konek, "select*from kategori_produk");
-						while ($kp = mysqli_fetch_array($katpro)) {
-						?>
-                <option value="<?php echo $kp['id']; ?>"><?php echo $kp['nama'] ?></option>
-                <?php } ?>
-            </select><br>
-            <br>
-            <label>Nama</label><br>
-            <input type="text" class="form-control" name="nama" value="<?php echo $data->nama; ?>"><br>
-            <label>Deskripsi</label><br>
-            <textarea class="form-control" name="deskripsi" required><?php echo $data->deskripsi; ?></textarea><br>
-            <label>Gambar</label><br>
-            <input type="file" class="form-control" name="gambar" required><br>
-            <label>Harga</label><br>
-            <input type="number" class="form-control" name="harga" required value="<?php echo $data->harga; ?>"><br>
-            <input type="submit" name="form-edit" value="Simpan" class="btn btn-success">
-        </form>
-    </div>
-    <div class="row col-md-12">
-        <hr>
-    </div>
-    <?php
-		}
-	}
-	?>
+        }
+    }
+    ?>
 
     <table class="table">
         <thead style="background:#00b4d8">
@@ -146,22 +146,17 @@ include "inc/header.php";
 
 
             <?php while ($data = mysqli_fetch_object($q)) { ?>
-            <tr>
-                <th style="vertical-align: middle" scope="row"><?php echo $no++; ?></th>
-                <td style="vertical-align: middle"><img src="<?php echo $url . 'uploads/' . $data->gambar ?>"
-                        width="100%"></td>
-                <td style="vertical-align: middle"><?php echo $data->nama ?></td>
-                <td style="vertical-align: middle"><?php echo number_format($data->harga, 0, ',', '.') ?></td>
-                <td style="vertical-align: middle">
-                    <a class="btn btn-sm btn-success" href="produk.php?act=edit&&id=<?php echo $data->id ?>">Edit <i
-                            class="fa-solid fa-pen-to-square"></i> </a>
-                    <a class="btn btn-sm btn-danger" href="produk.php?act=delete&&id=<?php echo $data->id ?>">Delete <i
-                            class="fa-solid fa-trash"></i> </a>
-                </td>
-            </tr>
+                <tr>
+                    <th style="vertical-align: middle" scope="row"><?php echo $no++; ?></th>
+                    <td style="vertical-align: middle"><img src="<?php echo $url . 'uploads/' . $data->gambar ?>" width="100%"></td>
+                    <td style="vertical-align: middle"><?php echo $data->nama ?></td>
+                    <td style="vertical-align: middle"><?php echo number_format($data->harga, 0, ',', '.') ?></td>
+                    <td style="vertical-align: middle">
+                        <a class="btn btn-sm btn-success" href="produk.php?act=edit&&id=<?php echo $data->id ?>">Edit <i class="fa-solid fa-pen-to-square"></i> </a>
+                        <a class="btn btn-sm btn-danger" href="produk.php?act=delete&&id=<?php echo $data->id ?>">Delete <i class="fa-solid fa-trash"></i> </a>
+                    </td>
+                </tr>
             <?php } ?>
         </tbody>
     </table>
 </div> <!-- /container -->
-
-<?php include "inc/footer.php"; ?>
